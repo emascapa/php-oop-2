5541cff34c75e2b93ce2fde22a926b3a29972f56
emascapa <emascapa@gmail.com>
2022-05-26 15:24:29 +0200

    Initial commit add class product

---
+++ Models/Product.php
@@ -0,0 +1,37 @@
+<?php 
+
+class Product {
+    protected $name;
+    protected $price;
+    protected $availability;
+
+    
+    function __construct(String $name, Float $price, Int $availability)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->availability = $availability;
+    }
+    
+    public function setName(String $newName) {
+        $this->name = $newName;    
+    }
+    public function getName() {
+        return $this->name;    
+    }
+    
+    public function setPrice(String $newPrice) {
+        $this->price = $newPrice;    
+    }
+    public function getPrice() {
+        return $this->price;    
+    }
+
+    public function setAvailability(String $newAvailability) {
+        $this->availability = $newAvailability;    
+    }
+    public function getAvailability() {
+        return $this->availability;    
+    }
+
+}
