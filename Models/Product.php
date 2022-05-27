9b8069586c50c54916423b06cf8dac453ce42d93
emascapa <emascapa@gmail.com>
2022-05-27 14:23:32 +0200

    add trait

--- Models/Product.php
+++ Models/Product.php
@@ -1,20 +1,10 @@
 <?php 
 
-class Product {
+trait BasicInfo {
     protected $name;
     protected $price;
     protected $availability;
 
-    protected $image = 'https://picsum.photos/seed/picsum/300/300';
-
-    
-    function __construct(String $name, Float $price, Int $availability)
-    {
-        $this->name = $name;
-        $this->price = $price;
-        $this->availability = $availability;
-    }
-    
     public function setName(String $newName) {
         $this->name = $newName;    
     }
@@ -35,6 +25,46 @@ class Product {
     public function getAvailability() {
         return $this->availability;    
     }
+
+}
+
+class Product {
+/*     protected $name;
+    protected $price;
+    protected $availability; */
+
+    use BasicInfo;
+
+    protected $image = 'https://picsum.photos/seed/picsum/300/300';
+
+    
+    function __construct(String $name, Float $price, Int $availability)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->availability = $availability;
+    }
+    
+/*     public function setName(String $newName) {
+        $this->name = $newName;    
+    }
+    public function getName() {
+        return $this->name;    
+    }
+    
+    public function setPrice(String $newPrice) {
+        $this->price = $newPrice;    
+    }
+    public function getPrice() {
+        return $this->price;    
+    }
+
+    public function setAvailability(String $newAvailability) {
+        $this->availability = $newAvailability;    
+    }
+    public function getAvailability() {
+        return $this->availability;    
+    } */
 
     public function getImage() {
         return $this->image;    
